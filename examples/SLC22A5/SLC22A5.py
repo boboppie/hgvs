22dc7ac75210c5b9e994d51517e24f8015909a35
Fengyuan Hu <fh293@cam.ac.uk>
2015-02-06 15:50:23 +0000

    run for SLC22A5

---
+++ examples/SLC22A5/SLC22A5.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+"""
+Example usage of HGVS library.
+
+To run the script, first begin by opening a terminal in the root directory
+of this software package. Note, the root directory should contain
+`setup.py`.
+
+Second, obtain genome sequence in FASTA format, which is required in
+example. Genome sequence can be fetched using the following commands:
+
+  wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/chromFa.tar.gz
+  tar zxvf chromFa.tar.gz
+  cat chr*.fa > hg19.fa
+  rm chr*.fa chromFa.tar.gz
+
+This example script can be run using:
+
+  python examples/example1.py
+
+The following output should be displayed:
+
+  chr11 17496508 T C
+  NM_000352.3(ABCC8):c.215A>G
+  ('NM_000352.3', 'c', '>', CDNACoord(215, -10), CDNACoord(215, -10), 'A', 'G')
+
+"""
+
+import pyhgvs as hgvs
+import pyhgvs.utils as hgvs_utils
+from pygr.seqdb import SequenceFileDB
+
+# Read genome sequence using pygr.
+# TODO file must be in current directory??? Workaround: create a sym link
+genome = SequenceFileDB('hg19.fa')
+
+# Read RefSeq transcripts into a python dict.
+with open('pyhgvs/data/genes.refGene') as infile:
+    transcripts = hgvs_utils.read_transcripts(infile)
+
+
+# Provide a callback for fetching a transcript by its name.
+def get_transcript(name):
+    return transcripts.get(name)
+
+
+# Parse the HGVS name into genomic coordinates and alleles.
+# REF http://www.arup.utah.edu/database/OCTN2/OCTN2_display.php
+# REF https://cgwb.nci.nih.gov/cgi-bin/hgTables?db=hg19&hgta_group=genes&hgta_track=refGene&hgta_table=refGene&hgta_doSchema=describe+table+schema
+# REF https://genome.ucsc.edu/FAQ/FAQtracks.html#tracks1
+
+with open('examples/SLC22A5/in.txt','r') as f:
+    with open('examples/SLC22A5/out.txt','w') as g: 
+        for x in f:
+            chrom, offset, ref, alt = hgvs.parse_hgvs_name(
+                x, genome, get_transcript=get_transcript)
+            print chrom, offset, ref, alt
+'''
+chrom, offset, ref, alt = hgvs.parse_hgvs_name(
+    'NM_003060.3:c.1A>G', genome, get_transcript=get_transcript)
+print chrom, offset, ref, alt
+# Returns variant in VCF style: ('chr11', 17496508, 'T', 'C')
+
+# Format an HGVS name.
+chrom, offset, ref, alt = ('chr11', 17496508, 'T', 'C')
+transcript = get_transcript('NM_000352.3')
+hgvs_name = hgvs.format_hgvs_name(
+    chrom, offset, ref, alt, genome, transcript)
+print hgvs_name
+# Returns 'NM_000352.3(ABCC8):c.215A>G'
+
+
+hgvs_name = hgvs.HGVSName('NM_000352.3:c.215-10A>G')
+# fields of the HGVS name are available as attributes:
+#
+# hgvs_name.transcript = 'NM_000352.3'
+# hgvs_name.kind = 'c'
+# hgvs_name.mutation_type = '>'
+# hgvs_name.cdna_start = hgvs.CDNACoord(215, -10)
+# hgvs_name.cdna_end = hgvs.CDNACoord(215, -10)
+# hgvs_name.ref_allele = 'A'
+# hgvs_name.alt_allele = 'G'
+
+print (hgvs_name.transcript,
+       hgvs_name.kind,
+       hgvs_name.mutation_type,
+       hgvs_name.cdna_start,
+       hgvs_name.cdna_end,
+       hgvs_name.ref_allele,
+       hgvs_name.alt_allele)
+'''
